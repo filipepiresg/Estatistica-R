351a716264987dd6392654da391bd7b421529f91
Filipe Pires <liipepires@gmail.com>
2018-10-03 17:27:41 -0300

    atividade 5 e tabela prova

---
+++ 2018.2/atividade5.R
@@ -0,0 +1,39 @@
+# 1
+# letra a)
+tamPop <- 1243088
+populacao <- numeric(tamPop)
+for( i in 1: 522097) {
+  populacao[i] <- 1;
+}
+
+proporcaoPopulacional <- sum(populacao)/tamPop
+
+# codigo em comum pra letra b)
+calculaProporcaoAmostral <- function(k, n, conf.level = 0.95) {
+  amostra <- c()
+  for(j in 1: k) {
+    amostra[j] <- (sum(sample(populacao, n, replace = F))/n)
+  }
+  sigma2 <- var(amostra) 
+  z <- qt((1+conf.level)/2, n-1) 
+  xbarra <- mean(amostra)
+  return(xbarra + c(-1,1)*z*sqrt(sigma2)/sqrt(n))
+}
+
+
+# letra b) i)
+amostrasTam100n100 <- calculaProporcaoAmostral(100, 100)
+amostrasTam100n2200 <- calculaProporcaoAmostral(100, 2200)
+
+amostrasTam100n100
+amostrasTam100n2200
+
+# letra b) ii)
+amostrasTam1000n100 <- calculaProporcaoAmostral(1000, 100)
+amostrasTam1000n2200 <- calculaProporcaoAmostral(1000, 2200)
+
+amostrasTam1000n100
+amostrasTam1000n2200
+
+
+proporcaoPopulacional
